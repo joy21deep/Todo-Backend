5a8ea00e096a8272f02d5ff31dff09b5d2dbde8b
joy21deep <joydeep.varenium@gmail.com>
2024-07-23 02:58:00 -0700

    complete push v1

--- routes/task.js
+++ routes/task.js
@@ -3,14 +3,18 @@ const router = express.Router();
 const taskController = require('../controller/task');
 const authMiddleware = require('../helper/check-auth');
 
+
 // Create a new task
 router.post('/create', authMiddleware, taskController.createTask);
 
 // Fetch tasks with date range
-router.get('/fetch', authMiddleware, taskController.getTasksByDateRange);
+router.post('/fetch', authMiddleware, taskController.getTasksByDateRange);
 
 // Update status for multiple tasks
-router.put('/update/status', authMiddleware, taskController.updateTaskStatus);
+router.post('/update/status', authMiddleware, taskController.updateTaskStatus);
+
+router.post('/dashboard', authMiddleware, taskController.dashboardData);
+
 
 // Update status for a single task
 // router.put('/tasks/status/single', authMiddleware, taskController.updateSingleTaskStatus);
